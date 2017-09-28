17ff648977fcd24d7473bc212180b0fb26c28fe2
Iraklis Dimitriadis <iraklis.dimitriadis@rwth-aachen.de>
2017-09-28 23:28:26 +0200

    added scrollbars to graph container

--- js/graph.js
+++ js/graph.js
@@ -88,57 +88,7 @@ function main(container)
                 source != state.text.node.getElementsByTagName('div')[0];
         };
 
-        // Adds scrollbars to the outermost div and keeps the
-        // DIV position and size the same as the vertex
-        var oldRedrawLabel = graph.cellRenderer.redrawLabel;
-        graph.cellRenderer.redrawLabel = function(state)
-        {
-            oldRedrawLabel.apply(this, arguments); // "supercall"
-            var graph = state.view.graph;
-            var model = graph.model;
 
-            if (model.isVertex(state.cell) && state.text != null)
-            {
-                // Scrollbars are on the div
-                var s = graph.view.scale;
-                state.text.node.style.overflow = 'hidden';
-                var div = state.text.node.getElementsByTagName('div')[0];
-
-                if (div != null)
-                {
-                    // Adds height of the title table cell
-                    var oh = 26;
-
-                    div.style.display = 'block';
-                    div.style.top = oh + 'px';
-                    div.style.width = Math.max(1, Math.round(state.width / s)) + 'px';
-                    div.style.height = Math.max(1, Math.round((state.height / s) - oh)) + 'px';
-
-                    // Installs the handler for updating connected edges
-                    if (div.scrollHandler == null)
-                    {
-                        div.scrollHandler = true;
-
-                        var updateEdges = mxUtils.bind(this, function()
-                        {
-                            var edgeCount = model.getEdgeCount(state.cell);
-
-                            // Only updates edges to avoid update in DOM order
-                            // for text label which would reset the scrollbar
-                            for (var i = 0; i < edgeCount; i++)
-                            {
-                                var edge = model.getEdgeAt(state.cell, i);
-                                graph.view.invalidate(edge, true, false);
-                                graph.view.validate(edge);
-                            }
-                        });
-
-                        mxEvent.addListener(div, 'scroll', updateEdges);
-                        mxEvent.addListener(div, 'mouseup', updateEdges);
-                    }
-                }
-            }
-        };
         // Enables rubberband selection
         new mxRubberband(graph);
 
