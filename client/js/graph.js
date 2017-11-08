d8c861a43c3d3b6a334865dfc25c23ea47145512
Iraklis Dimitriadis <iraklis.dimitriadis@rwth-aachen.de>
2017-11-08 14:54:09 +0100

    add multiple anchor points to node (class)

--- client/js/graph.js
+++ client/js/graph.js
@@ -84,6 +84,42 @@ function main(container) {
         // Enables rubberband selection
         new mxRubberband(graph);
 
+        /*
+           =======================================
+                      NODE ANCHOR SETUP
+           =======================================
+
+        */
+        mxGraph.prototype.getAllConnectionConstraints = function (terminal, source) {
+            if (terminal != null && terminal.shape != null) {
+                if (terminal.shape.stencil != null) {
+                    if (terminal.shape.stencil != null) {
+                        return terminal.shape.stencil.constraints;
+                    }
+                }
+                else if (terminal.shape.constraints != null) {
+                    return terminal.shape.constraints;
+                }
+            }
+
+            return null;
+        };
+        mxShape.prototype.constraints = [new mxConnectionConstraint(new mxPoint(0.25, 0), true),
+            new mxConnectionConstraint(new mxPoint(0.5, 0), true),
+            new mxConnectionConstraint(new mxPoint(0.75, 0), true),
+            new mxConnectionConstraint(new mxPoint(0, 0.25), true),
+            new mxConnectionConstraint(new mxPoint(0, 0.5), true),
+            new mxConnectionConstraint(new mxPoint(0, 0.75), true),
+            new mxConnectionConstraint(new mxPoint(1, 0.25), true),
+            new mxConnectionConstraint(new mxPoint(1, 0.5), true),
+            new mxConnectionConstraint(new mxPoint(1, 0.75), true),
+            new mxConnectionConstraint(new mxPoint(0.25, 1), true),
+            new mxConnectionConstraint(new mxPoint(0.5, 1), true),
+            new mxConnectionConstraint(new mxPoint(0.75, 1), true),
+            new mxConnectionConstraint(new mxPoint(1, 0), true),
+            new mxConnectionConstraint(new mxPoint(0, 1), true),
+            new mxConnectionConstraint(new mxPoint(1, 1), true),
+            new mxConnectionConstraint(new mxPoint(0, 0), true)];
 
         /*
            =======================================
