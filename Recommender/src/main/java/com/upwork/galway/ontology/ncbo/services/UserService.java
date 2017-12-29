2a0c714c44fbc913059927226fd690addc9d6fcb
md.rezaul.karim <md.rezaul.karim@fit.fraunhofer.de>
2017-12-29 05:45:36 -0800

    remove unwanted folders

--- Recommender/src/main/java/com/upwork/galway/ontology/ncbo/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.fit.neologism.recommender.ontology.ncbo.services;
-
-import org.fit.neologism.recommender.ontology.ncbo.models.User;
-
-public interface UserService {
-    User findUserByEmail(String email);
-    void saveUser(User user);
-}
