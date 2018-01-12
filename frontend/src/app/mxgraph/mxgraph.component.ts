d4102a740b13a3b9baa261b1f4150572662d2fa5
Lars Gleim <gleim@cs.rwth-aachen.de>
2018-01-12 11:47:27 +0100

    Refactored MxGraphCode to external Class

--- frontend/src/app/mxgraph/mxgraph.component.ts
+++ frontend/src/app/mxgraph/mxgraph.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import mxgraphFactory, { mxgraph as mx } from 'mxgraph';
-import "rxjs/add/operator/take";
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import 'rxjs/add/operator/take';
 
+import { MxgraphService } from './mxgraph';
 import { N3Codec } from './N3Codec';
 
 @Component({
@@ -9,75 +9,42 @@ import { N3Codec } from './N3Codec';
     templateUrl: './mxgraph.component.html',
     styleUrls: ['./mxgraph.component.css'],
 })
-export class MxgraphComponent implements OnInit {
+export class MxgraphComponent implements OnInit, OnDestroy {
     @ViewChild('view') mxGraphView: ElementRef;
-
-    mx = mxgraphFactory({
-        mxImageBasePath: 'mxgraph/images',
-        mxBasePath: 'mxgraph',
-    });
-    mxGraph = this.mx.mxGraph;
-    mxClient = this.mx.mxClient;
-    mxUtils = this.mx.mxUtils;
-    mxRubberband = this.mx.mxRubberband;
-
-    graph: mx.mxGraph;
-    canvas: mx.mxCell;
-    container: HTMLDivElement;
+    private mx: MxgraphService;
 
     constructor() { }
 
-    addVertex(
-        id: string,
-        x: number = Math.random() * this.container.clientWidth,
-        y: number = Math.random() * this.container.clientHeight,
-        w: number = 80,
-        h: number = 30,
-    ) {
-        return this.graph.insertVertex(this.canvas, id, id, x, y, w, h);
+    ngOnInit() {
+        this.mx = new MxgraphService(this.mxGraphView.nativeElement);
+
+        // Adds cells to the model in a single step
+        // this.graph.getModel().beginUpdate();
+        try {
+            const codec = new N3Codec();
+            codec.parseUrl('http://xmlns.com/foaf/spec/index.rdf')
+                .take(50)
+                .subscribe(([e, triple, prefixes]) => {    // Todo: Unsubscribe on delete
+                    if (triple) {
+                        console.log(triple.subject, triple.predicate, triple.object, '.');
+
+                        this.mx.graph.getModel().beginUpdate();
+                        const v1 = this.mx.addVertex(triple.subject);
+                        const v2 = this.mx.addVertex(triple.object);
+                        this.mx.graph.insertEdge(this.mx.canvas, triple.predicate, triple.predicate, v1, v2);
+                        this.mx.graph.getModel().endUpdate();
+                    } else {
+                        console.log('# That\'s all, folks!', prefixes);
+                    }
+                });
+        }
+        finally {
+            // Updates the display
+            // this.graph.getModel().endUpdate();
+        }
     }
 
-    ngOnInit() {
-        // Checks if the browser is supported
-        if (!this.mxClient.isBrowserSupported()) {
-            this.mxUtils.error('Browser is not supported!', 200, false);
-        } else {
-            this.container = this.mxGraphView.nativeElement;
-
-            // Creates the graph inside the given container
-            this.graph = new this.mxGraph(this.container);
-
-            // Enables rubberband selection - Weird constructor side effect stuff
-            const rubberband = new this.mxRubberband(this.graph);
-
-            // Gets the default parent for inserting new cells. This
-            // is normally the first child of the root (ie. layer 0).
-            this.canvas = this.graph.getDefaultParent();
-
-            // Adds cells to the model in a single step
-            // this.graph.getModel().beginUpdate();
-            try {
-                const codec = new N3Codec();
-                codec.parseUrl('http://xmlns.com/foaf/spec/index.rdf')
-                    .take(50)
-                    .subscribe(([e, triple, prefixes]) => {    // Todo: Unsubscribe on delete
-                        if (triple) {
-                            console.log(triple.subject, triple.predicate, triple.object, '.');
-
-                            this.graph.getModel().beginUpdate();
-                            const v1 = this.addVertex(triple.subject);
-                            const v2 = this.addVertex(triple.object);
-                            this.graph.insertEdge(this.canvas, triple.predicate, triple.predicate, v1, v2);
-                            this.graph.getModel().endUpdate();
-                        } else {
-                            console.log('# That\'s all, folks!', prefixes);
-                        }
-                    });
-            }
-            finally {
-                // Updates the display
-                // this.graph.getModel().endUpdate();
-            }
-        }
+    ngOnDestroy() {
+        this.mx.destroy();
     }
 }
