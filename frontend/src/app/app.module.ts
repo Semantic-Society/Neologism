322833cae0b6b8ff3b674392be09beda1f905cef
Iraklis Dimitriadis <iraklis.dimitriadis@rwth-aachen.de>
2018-03-07 14:24:19 +0100

    add list to infobox component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
 // App Services
 // import { RdfmodelService } from './services/rdfmodel.service';
@@ -29,7 +30,8 @@ import { InfoboxComponent } from './infobox/infobox.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatExpansionModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [
     // RdfmodelService
