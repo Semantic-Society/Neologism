f6c07d42603ed4336f12d32bd5632c89870e5ac1
michaelcochez <michael.cochez@>
2018-03-20 19:20:43 +0100

    Gave more reaosnable defaults for the metdata panel and moved the data from hardcoded in the html to harcoded in the typescript components. Removed the 'click me!' link.

--- frontend/src/app/metadata/metadata.component.ts
+++ frontend/src/app/metadata/metadata.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetadataComponent implements OnInit {
 
+  vocabularyName = 'Vocabulary Name';
+  vocabularyHref = '#';
+  vocabularyLinkText = 'Persistent link to the vocabulary';
+  versionString = 'Version: 0.0';
+
+  authors: string[] = ['Author 1', 'Author 2', 'Author 3', 'Author 4', 'Author 5', 'Author 6', 'Author 7',];
+
   constructor() { }
 
   ngOnInit() {
