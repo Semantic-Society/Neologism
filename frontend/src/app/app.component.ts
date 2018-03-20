53032bfa7c59a8687a1789941d18d947d13d86ca
michaelcochez <michael.cochez@>
2018-03-20 18:18:31 +0100

    Added more sensible defaults and fixed issues fromt he linter

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +8,25 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class AppComponent {
   constructor() {
   }
-  editMode: Number = 0;
+  editMode = 0;
   title = 'app';
-  currentLabel:String ="dcat:Catalog";
-  currentIdentifier:String = "http://www.w3.org/ns/dcat#Catalog";
-  currentDescrpition:String = "Some Awesome Description";
+  currentLabel = 'Type query here ...';
+  currentIdentifier = '';
+  currentDescrpition = '';
 
-
-  toggleEditMode(event?){
+  toggleEditMode(event?) {
     this.currentLabel = event;
-    if (this.editMode == 1) {
-      this.editMode=0;
-    }else{
-      this.editMode=1;
+    if (this.editMode === 1) {
+      this.editMode = 0;
+    } else {
+      this.editMode = 1;
     }
   }
 
-  updateCurrentLabel(event){
-    console.log("New label name is: " +event);
+  updateCurrentLabel(event) {
+    console.log('New label name is: ' + event);
 
-    this.currentLabel=event;
+    this.currentLabel = event;
   }
 
 }
