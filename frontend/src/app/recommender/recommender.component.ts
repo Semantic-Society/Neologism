1cab44d52f1db7e9d6a8ef76568a92f22621ad1f
Iraklis Dimitriadis <rakli@comtip.net>
2018-04-06 20:19:45 +0200

    try to fix issue #50

--- frontend/src/app/recommender/recommender.component.ts
+++ frontend/src/app/recommender/recommender.component.ts
@@ -1,122 +1,148 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import 'rxjs/add/operator/map';
-import { MxgraphService } from '../mxgraph/mxgraph';
-import { RecommendationService } from '../services/recommendation.service';
+import {MxgraphService} from '../mxgraph/mxgraph';
+import {RecommendationService} from '../services/recommendation.service';
 
 interface IClassRecommendation {
-    uri: string;
-    comment: string;
-    label: string;
-    creator: string;
+  uri: string;
+  comment: string;
+  label: string;
+  creator: string;
 }
+
 interface IPropertyRecommendation {
-    uri: string;
-    comment: string;
-    label: string;
-    range: string;
+  uri: string;
+  comment: string;
+  label: string;
+  range: string;
 }
 
 interface IClassRecommendation {
-    uri: string;
-    comment: string;
-    label: string;
-    creator: string;
+  uri: string;
+  comment: string;
+  label: string;
+  creator: string;
 }
+
 interface IPropertyRecommendation {
-    uri: string;
-    comment: string;
-    label: string;
-    range: string;
+  uri: string;
+  comment: string;
+  label: string;
+  range: string;
 }
 
 @Component({
-    selector: 'app-recommender',
-    templateUrl: './recommender.component.html',
-    styleUrls: ['./recommender.component.css'],
+  selector: 'app-recommender',
+  templateUrl: './recommender.component.html',
+  styleUrls: ['./recommender.component.css'],
 })
 export class RecommenderComponent implements OnInit {
-    // TODO REFACTOR THIS
+  // TODO REFACTOR THIS
 
-    // TODO MC: I don't know a better way to ensure that an old finishing request does not turn of the spinner OR fills the results.
-    // TODO before it was be possible that an old, still pending request which returns turns off the spinner or fills the result list
-    private currentRequestNumber = 0;
+  // TODO MC: I don't know a better way to ensure that an old finishing request does not turn of the spinner OR fills the results.
+  // TODO before it was be possible that an old, still pending request which returns turns off the spinner or fills the result list
+  private currentRequestNumber = 0;
 
-    constructor(private recommendationService: RecommendationService) { }
+  constructor(private recommendationService: RecommendationService) {
+  }
 
-    @Input() mx: MxgraphService;
-    @ViewChild('labelInput') labelInput: ElementRef;
+  @Input() mx: MxgraphService;
+  @ViewChild('labelInput') labelInput: ElementRef;
 
-    protected showSpinner = false;
-    recommendations: IClassRecommendation[] = [];
-    selectedRecommendation: IClassRecommendation;
-    isSelectedRecommendationVisible = false;
-    propertyRecommendations: IPropertyRecommendation[] = [];
+  protected showSpinner = false;
+  protected showSpinnerProp = false;
+  protected noPropertiesFound = false;
+  recommendations: IClassRecommendation[] = [];
+  selectedRecommendation: IClassRecommendation;
+  isSelectedRecommendationVisible = false;
+  propertyRecommendations: IPropertyRecommendation[] = [];
 
-    ngOnInit() {
-        this.selectLabelInput();
+  ngOnInit() {
+    this.selectLabelInput();
+  }
+
+  selectLabelInput() {
+    const labelField = this.labelInput.nativeElement as HTMLInputElement;
+    // labelField.value = this.inputLabel; // Workaround hack to preselect the input field text
+    // since the value is not set immediately by angular before labelField.select
+    labelField.select();
+  }
+
+  public sendInputLabel(): void {
+    this.recommendations = [];
+    this.isSelectedRecommendationVisible = false;
+    const queryString: string = (this.labelInput.nativeElement as HTMLInputElement).value;
+    if (queryString === '') {
+      return;
     }
-    selectLabelInput() {
-        const labelField = this.labelInput.nativeElement as HTMLInputElement;
-        // labelField.value = this.inputLabel; // Workaround hack to preselect the input field text
-        // since the value is not set immediately by angular before labelField.select
-        labelField.select();
-    }
-
-    public sendInputLabel(): void {
-        this.recommendations = [];
-        this.isSelectedRecommendationVisible = false;
-        const queryString: string = (this.labelInput.nativeElement as HTMLInputElement).value;
-        if (queryString === '') {
-            return;
-        }
-        const exp = () => {
-            this.currentRequestNumber++;
-            const requestNumber = this.currentRequestNumber;
-            this.enableSpinner();
-            if (!(this.mx && queryString)) {
-                throw new Error('mx or labelFiled null');
+    const exp = () => {
+      this.currentRequestNumber++;
+      const requestNumber = this.currentRequestNumber;
+      this.enableSpinner();
+      if (!(this.mx && queryString)) {
+        throw new Error('mx or labelFiled null');
+      }
+      this.mx.serializeModel().then((model) =>
+        this.recommendationService.classRecommendationforNewClass(model, queryString)
+          .subscribe(
+            (recs) => {
+              if (this.currentRequestNumber === requestNumber) {
+                this.recommendations = recs;
+              }
+            }, null // TODO handle error?
+            , () => {
+              if (this.currentRequestNumber === requestNumber) {
+                this.disableSpinner();
+              }
             }
-            this.mx.serializeModel().then((model) =>
-                this.recommendationService.classRecommendationforNewClass(model, queryString)
-                    .subscribe(
-                        (recs) => {
-                            if (this.currentRequestNumber === requestNumber) {
-                                this.recommendations = recs;
-                            }
-                        }, null // TODO handle error?
-                        , () => {
-                            if (this.currentRequestNumber === requestNumber) {
-                                this.disableSpinner();
-                            }
-                        }
-                    ));
-        };
-        exp();
-        /*
-        this.onInputLabelUpdated.emit(labelField.value);
-        console.log("input typing event fired! New label name "+ labelField.value);*/
-    }
+          ));
+    };
+    exp();
+    /*
+    this.onInputLabelUpdated.emit(labelField.value);
+    console.log("input typing event fired! New label name "+ labelField.value);*/
+  }
 
-    selectRecommendation(r: IClassRecommendation) {
-        this.selectedRecommendation = r;
-        this.isSelectedRecommendationVisible = true;
-        this.recommendationService.propertyRecommendation(r.uri, r.creator)
-            .subscribe((res) => {
-                this.propertyRecommendations = res;
-                // console.log('Received Property Recommendation', res);
-            });
-    }
+  selectRecommendation(r: IClassRecommendation) {
+    console.log("Send request");
+    this.enableSpinnerProp();
+    this.selectedRecommendation = r;
+    this.isSelectedRecommendationVisible = true;
+    this.recommendationService.propertyRecommendation(r.uri, r.creator)
+      .subscribe((res) => {
+        this.propertyRecommendations = res;
+        // console.log('Received Property Recommendation', res);
 
-    addToGraph() {
-        console.log('recommenderComponent -> addToGraph:', this.selectedRecommendation.uri, this.selectedRecommendation.label, this.selectedRecommendation.creator);
-        this.mx.insertClass(this.selectedRecommendation.uri, this.selectedRecommendation.label, this.selectedRecommendation.creator);
-        this.mx.selectClass(this.selectedRecommendation.uri);
-    }
 
-    enableSpinner() {
-        this.showSpinner = true;
-    }
-    disableSpinner() {
-        this.showSpinner = false;
-    }
+      }, null
+      , ()=> {
+          console.log("request finished")
+          this.disableSpinnerProp();
+          if(this.propertyRecommendations == []) {
+            this.noPropertiesFound = false;
+          }
+        });
+  }
+
+  addToGraph() {
+    console.log('recommenderComponent -> addToGraph:', this.selectedRecommendation.uri, this.selectedRecommendation.label, this.selectedRecommendation.creator);
+    this.mx.insertClass(this.selectedRecommendation.uri, this.selectedRecommendation.label, this.selectedRecommendation.creator);
+    this.mx.selectClass(this.selectedRecommendation.uri);
+  }
+
+  enableSpinner() {
+    this.showSpinner = true;
+  }
+
+  disableSpinner() {
+    this.showSpinner = false;
+  }
+
+  enableSpinnerProp() {
+    this.showSpinnerProp = true;
+  }
+
+  disableSpinnerProp() {
+    this.showSpinnerProp = false;
+  }
 }
