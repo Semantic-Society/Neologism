b4c43f513bed4bf94d5eb34c031513eb69445dc3
michaelcochez <michael.cochez@>
2018-06-13 08:33:32 +0200

    Seems like this is the way to go for adding a class

--- frontend/api/server/methods.ts
+++ frontend/api/server/methods.ts
@@ -1,5 +1,5 @@
 import { Classes, Vocabularies } from '../collections';
-import {meteorID} from '../models';
+import { meteorID } from '../models';
 
 Meteor.methods({
   'vocabulary.create'({ name, authors, description, uriPrefix }) {
@@ -12,10 +12,18 @@ Meteor.methods({
     // TODO: Sanitize
     Vocabularies.remove(vocabId);
   },
-  'class.create'({vocabId, name, description, URI}) {
+  'class.create'({ vocabId, name, description, URI }) {
     // TODO: Sanitize
-    const classId = Classes.insert({name, description, URI, properties : [], position: {x: 0, y: 0}, skos: {closeMatch: [], exactMatch: []}});
-    Vocabularies.update({_id: vocabId}, { $push: {classes: classId}});
+    // const classId = Classes.insert({ name, description, URI, properties: [], position: { x: 0, y: 0 }, skos: { closeMatch: [], exactMatch: [] } });
+    // Vocabularies.update({ _id: vocabId }, { $push: { classes: classId } });
+
+    // TODO: is the following implmentation any better:
+
+    const classIdO = Classes.insert({ name, description, URI, properties: [], position: { x: 0, y: 0 }, skos: { closeMatch: [], exactMatch: [] } });
+    classIdO.take(1).map((theID) => {
+      Vocabularies.update({ _id: vocabId }, { $push: { classes: theID } });
+    } );
+
   },
 
 });
