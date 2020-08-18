d34efc745bff911f8c608790f65aa6745d2ad41b
JohannesLipp <johanneslipp@freenet.de>
2020-08-18 16:21:18 +0200

    Update frontend/docker/build-angular-bundle.sh

--- frontend/docker/build-angular-bundle.sh
+++ frontend/docker/build-angular-bundle.sh
@@ -10,7 +10,7 @@ cd $APP_SOURCE_FOLDER
 
 printf "\n[-] Building Meteor Client bundle...\n\n"
 
-npm_config_root_url=$APP_ROOT_URL  meteor npm run meteor-client:bundle-server2
+npm_config_root_url=$APP_ROOT_URL  meteor npm run meteor-client:bundle-server
 
 printf "\n[-] Building Angular bundle...\n\n"
 
