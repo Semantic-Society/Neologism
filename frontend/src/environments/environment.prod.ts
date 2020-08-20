99890f12680f952e6b7fb2291c5fed2be07c51ff
HA <>
2020-08-20 13:36:55 +0200

    update recommender url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   recommender:{
-    base:"http://recommender",
-    port:"8080",
+    base:"http://cloud33.dbis.rwth-aachen.de",
+    port:"",
   } 
 };
