dcae4ffa81c21184b731c3a3803282f1189a04f9
hussain.ali <>
2021-01-23 14:35:34 +0100

    init

--- frontend/api/server/main.ts
+++ frontend/api/server/main.ts
@@ -1,6 +1,14 @@
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 Meteor.startup(() => {
+  // Listen to incoming HTTP requests, can only be used on the server
+WebApp.rawConnectHandlers.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type");
+  return next();
+});
+
   // code to run on server at startup
  Accounts.config({
    forbidClientAccountCreation: true
