04a802ebeb47efb7e9bd4cbb73e4a451e8e07ff4
Enis <eniszej@gmail.com>
2021-05-16 14:42:37 +0200

    added methods to metadata comp

--- app/src/app/metadata/metadata.component.ts
+++ app/src/app/metadata/metadata.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { domain } from 'process';
 
+import { MeteorObservable, zoneOperator } from "meteor-rxjs";
 import { Vocabularies } from '../../../api/collections';
 import { IvocabularyExtended } from '../../../api/models';
+import { VocabulariesService } from '../services/vocabularies.service';
 
 @Component({
   selector: 'app-metadata',
@@ -10,7 +13,27 @@ import { IvocabularyExtended } from '../../../api/models';
 })
 export class MetadataComponent {
   @Input() vocabulary: IvocabularyExtended;
+  domain:string
+  public editing:boolean = false;
+  constructor( 
 
-  constructor() { }
+    private vocabService: VocabulariesService,
+  ){
+  }
 
+  editDomain(){
+    MeteorObservable.call("vocabulary.addDomain",this.domain,this.vocabulary._id).pipe(zoneOperator())
+    .subscribe((_response) => {
+      // Handle success and response from server!
+    }, (err) => {
+      console.log(err);
+      // Handle error
+    });;
+    console.log(this.vocabService.getVocabulary(this.vocabulary._id))
+      this.editing = false
+      console.log(this.vocabulary)
+  }
+  startEdit(){
+    this.editing=!this.editing;
+  }
 }
