95d7deee449fedb2d6739480cdd0bedc03807daf
hussain.ali <>
2021-11-19 11:26:45 +0100

    removed extra ns

--- app/src/app/mxgraph/N3Codec.ts
+++ app/src/app/mxgraph/N3Codec.ts
@@ -14,7 +14,6 @@ export class N3Codec {
         rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
         rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
         owl: 'http://www.w3.org/2002/07/owl#',
-        xmlns: 'http://www.w3.org/2001/XMLSchema#',
         purl: 'http://purl.org/dc/terms/',
         proxivocab: 'http://hussain.ali.gitlab.io/vocab-proximity/'
     };
