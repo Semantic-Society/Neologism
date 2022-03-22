93fcbcf39b7544e6df08102047e58b4eb9ff1713
Hussain Ali <hussain.ali-cs@outlook.com>
2022-03-22 23:46:34 +0100

    mongo script corrected reuse env

--- docker/mongoseed.sh
+++ docker/mongoseed.sh
@@ -9,9 +9,9 @@ then
   printf "Data already seeded!\n"
   printf "If you want to remove and reseed, run 'rm /scripts/.seeddata'\n\n"
 else  
-    printf "\nImporting Document in Database called neologism\n"
+    printf "\nImporting Document in Database\n"
     printf "===================================================\n"
-    mongoimport --uri $MONGO_URL --db neologism --collection users --file $SCRIPTS_FOLDER/guest.json
+    mongoimport --uri $MONGO_URL --collection users --file $SCRIPTS_FOLDER/guest.json
   touch $SCRIPTS_FOLDER/.seeddata
   printf "\nDONE!\n"
 fi 
